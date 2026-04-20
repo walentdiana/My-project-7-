85be103aa0f1b5be7d6fd9a1398407035f3f121f
Dziana <walent17072003@gmail.com>
2026-04-20 23:01:07 +0200

    Initial commit

---
+++ Assets/Scripts/Dz.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class Dz : MonoBehaviour
+{
+    private void Start()
+    {
+        int[] enemyHealth = new int[14] {15,60,78,34,155,344,2,68,20,59,60,67,91,9};
+        
+        int max = enemyHealth[0];
+
+         
+        foreach (var i in enemyHealth)
+        {
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+        
+        
+        if (max >= 100)
+        {
+            Debug.Log($"Босс со здоровьем {max}, земля тебе металлом дружище!");
+        }
+        else if (max < 50)
+        {
+            Debug.Log($"Обычный со здоровьем {max}");
+        }
+        else
+        {
+            Debug.Log($"Элитный со здоровьем {max}");
+        }
+         vvvvvv
+    }
+}
