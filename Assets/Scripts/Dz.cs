1c1b5d4f6fc5c7557ab1513596eba3f8b891af59
Dziana <walent17072003@gmail.com>
2026-04-21 00:31:02 +0200

    Zadanie4
    Не совсем разобралась как сделать награды через второй массив, сделала все в одном.

--- Assets/Scripts/Dz.cs
+++ Assets/Scripts/Dz.cs
@@ -5,49 +5,74 @@ using UnityEngine;
 public class Dz : MonoBehaviour
 {
     private void Start()
-    {   
+    {
+        /*Задание 4
+        int[] countEnemyInWaves = new int[15] { 3, 2, 16, 7, 9, 23, 5, 2, 1, 70, 8, 5, 4, 45, 6 };
+
+        int allEnemys = countEnemyInWaves.Sum();
         
+        int points = 0;
+
         
+        if (allEnemys > 30)
+        {
+            Debug.Log("Сложный уровень");
+        }
+        else
+        {
+            Debug.Log("Легкий уровень");
+        }
         
+
+        int countEasyWave = 0;
+        int countNormalWave = 0;
+        int countHardWave = 0;
+
+        foreach (var c in countEnemyInWaves)
+        {
+            if (c <= 5)
+            {
+                countEasyWave = countEasyWave + 1;
+                points = points + 1; 
+            }
+            else if (c >= 6 && c <= 10)
+            {
+                countNormalWave = countNormalWave + 1;
+                points = points + 2;
+            }
+            else if (c >= 11)
+            {
+                countHardWave = countHardWave + 1; 
+                points = points + 3;
+            }
+        }
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        Debug.Log($"Количесво легких волн {countEasyWave}");
+        Debug.Log($"Количество средних волн {countNormalWave}");
+        Debug.Log($"Количество сложных волн {countHardWave}");
+        Debug.Log(points);*/
         
         
         /*Задание 3
-        int[] items = new int[9] {12,77,3,90,5,65,99,44,8}; 
-        
+        int[] items = new int[9] {12,77,3,90,5,65,99,44,8};
+
         int overallvalue = items.Sum();
-        
+
         Debug.Log($"Общая ценность инвенторя {overallvalue}");
-        
-        
+
+
         int numberOfRareItems = 0;
-        
+
         foreach (var i in items)
         {
-            if (i > 70) 
+            if (i > 70)
             {
                 numberOfRareItems = numberOfRareItems + 1;
             }
         }
         Debug.Log($"Количество редких предметов {numberOfRareItems}");
-        
-        
+
+
         if (numberOfRareItems > 2)
         {
             Debug.Log($"Вы Богатый игрок");
@@ -56,8 +81,8 @@ public class Dz : MonoBehaviour
         {
             Debug.Log($"Вы Бедный игрок");
         }*/
-        
-        
+
+
         /*Задание 2
         int[] enemyHealth = new int[14] {15,60,78,34,155,344,2,68,20,59,60,67,91,9};
 
