7619c43d518aecec53036c6699ce468947562d38
Dziana <walent17072003@gmail.com>
2026-04-23 20:17:02 +0200

    Update Dz.cs

--- Assets/Scripts/Dz.cs
+++ Assets/Scripts/Dz.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using UnityEngine;
 
 public class Dz : MonoBehaviour
@@ -8,6 +7,7 @@ public class Dz : MonoBehaviour
     {
         /*Задание 5
         int[] array = new int[15] { 5, 7, 8, 2, 1, 3, 13, 15, 11, 14, 4, 6, 9, 10, 12 };
+        int[] sortArray = new int[array.Length];
 
         string unsorted = string.Join(", ", array);
         Debug.Log(unsorted);
@@ -17,7 +17,7 @@ public class Dz : MonoBehaviour
             for (int j = 0; j < array.Length - i - 1; j++)
             {
                 if (array[j] > array[j + 1])
-                {-=
+                {
                     int k = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = k;
@@ -77,10 +77,15 @@ public class Dz : MonoBehaviour
 
         /*Задание 3
         int[] items = new int[9] {12,77,3,90,5,65,99,44,8};
+        
+        int sum = 0;
 
-        int overallvalue = items.Sum();
+           foreach (int score in items)
+           {
+               sum += score;
+           }
 
-        Debug.Log($"Общая ценность инвенторя {overallvalue}");
+        Debug.Log($"Общая ценность инвенторя {sum}");
 
 
         int numberOfRareItems = 0;
@@ -122,7 +127,7 @@ public class Dz : MonoBehaviour
 
         if (max >= 100)
         {
-            Debug.Log($"Босс со здоровьем {max}, земля тебе металлом дружище!");
+            Debug.Log($"Босс со здоровьем {max}");
         }
         else if (max < 50)
         {
@@ -137,15 +142,23 @@ public class Dz : MonoBehaviour
         /*Задание 1
 
         int[] scores = new int[6] { 29, 80, 2, 13, 9, 44 };
+        
+        int sum = 0;
 
-        int total = 0;
+        foreach (int score in scores)
+        {
+            sum += score;
+        }
+
+        int middle = sum / scores.Length;
+
 
         foreach (var b in scores)
         {
-            total += b;
+            middle += b;
         }
 
-        if (total >= 50)
+        if (middle >= 50)
         {
             Debug.Log("Победа");
         }
